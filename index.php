e9d9c7ffa6550e6b9900339a283a45606518f7ca
Wara Massa <waramassa70@gmail.com>
2020-11-24 23:09:15 +0100

    php3 exo1

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+            // nouvelle manière 
+$months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "décembre"];
+            // ancienne manière
+$months = array ("Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "décembre");
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP3 exo1</title>
+</head>
+<body>
+    
+</body>
+</html>
